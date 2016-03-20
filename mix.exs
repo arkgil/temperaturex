e6bce503bfb1a55039d15a197ae1cf2e0763c703
zsuidakra <arkadiusz.gil95@gmail.com>
2016-03-20 15:03:31 +0100

    Add HTTPoison as dependency

--- mix.exs
+++ mix.exs
@@ -14,7 +14,10 @@ defmodule Temperaturex.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [
+      :logger,
+      :httpoison
+    ]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,8 +31,9 @@ defmodule Temperaturex.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      { :ex_doc, "~> 0.11"},
-      { :earmark, ">= 0.0.0"}
+      {:ex_doc, "~> 0.11"},
+      {:earmark, ">= 0.0.0"},
+      {:httpoison, "~> 0.8"}
     ]
   end
 end
