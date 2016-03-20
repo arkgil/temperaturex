a11e2ccdf8b943f7ba1f68d6a729dbce4da36fac
zsuidakra <arkadiusz.gil95@gmail.com>
2016-03-20 14:48:37 +0100

    Test process function

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,6 +1,7 @@
 defmodule CLITest do
   use ExUnit.Case
-  import Temperaturex.CLI, only: [parse_args: 1]
+  import ExUnit.CaptureIO
+  import Temperaturex.CLI, only: [parse_args: 1, process: 1]
 
   test ":help is returned when parsing -h or --help options" do
     assert parse_args(["--help"]) == :help
@@ -14,5 +15,10 @@ defmodule CLITest do
   test ":help is returned when two or more options are given" do
     assert parse_args(["1", "2", "3"]) == :help
     assert parse_args(["1", "2"]) == :help
+  end
+
+  test "help message is displayed when help flags are given" do
+    fun = fn -> process(:help) end
+    assert capture_io(fun) == "usage: tempx <station name>\n\n"
   end
 end
