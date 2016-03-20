004df70ca763b3a06d8cc461c5bf1b6c7b45842c
zsuidakra <arkadiusz.gil95@gmail.com>
2016-03-20 13:40:35 +0100

    Add docs-generating dependencies

--- mix.exs
+++ mix.exs
@@ -27,6 +27,9 @@ defmodule Temperaturex.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      { :ex_doc, "~> 0.11"},
+      { :earmark, ">= 0.0.0"}
+    ]
   end
 end
