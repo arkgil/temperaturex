acc22ae7cd39dda3761673bc8bfb24efd4827c12
zsuidakra <arkadiusz.gil95@gmail.com>
2016-03-20 15:33:09 +0100

    Handle api responses

--- lib/temperaturex/weather_conditions.ex
+++ lib/temperaturex/weather_conditions.ex
@@ -1,4 +1,5 @@
-defmodule WeatherConditions do
+defmodule Temperaturex.WeatherConditions do
+  require Logger
   @moduledoc """
   This module provides functionality for fetching weather condition
   data from US NOAA API.
@@ -8,9 +9,24 @@ defmodule WeatherConditions do
   @doc """
   Fetches weather condition data.
   """
+  @spec fetch(String.t) :: tuple
   def fetch(station) do
-    response = HTTPoison.get("#{@noaa_url}/#{station}.xml")
-    response
+    data_url = "#{@noaa_url}/#{station}.xml"
+    Logger.info "Fetching data from #{data_url}"
+    HTTPoison.get(data_url) |> handle
+  end
+
+  @doc """
+  Handles response returned by NOAA's API.
+  """
+  @spec handle(tuple) :: tuple
+  def handle({:ok, %{status_code: 200, body: body}}) do
+    {:ok, body}
+  end
+
+  def handle({_, %{status_code: status, body: body}}) do
+    Logger.error "Error: #{status} returned"
+    {:error, body}
   end
 
 end
