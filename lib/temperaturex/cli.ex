0017e4591cbf91f1a72c79aaa62053756ab6bbf1
zsuidakra <arkadiusz.gil95@gmail.com>
2016-03-20 13:23:18 +0100

    Add docs for main/1 and parse_args/1

--- lib/temperaturex/cli.ex
+++ lib/temperaturex/cli.ex
@@ -1,16 +1,34 @@
 defmodule Temperaturex.CLI do
+  @moduledoc """
+  CLI module provides functionality of taking and parsing
+  command line arguments, and processing these arguments
+  using functions defined in other modules
+  """
 
+  @doc """
+  Main application function, it is invoked on application start
+  and gets passed command line arguments.
+  """
+  @spec main(list) :: nil
   def main(argv) do
     argv
     |> parse_args
+    nil
   end
 
+  @doc """
+  `argv` is a list containing command line arguments.
+  Possible argument values are:
+  * -h or --help in which case it returns :help
+  * weather station name, in which case given name is returned
+  * otherwise returns :help
+  """
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
     case parse do
       { [help: true], _, _ } -> :help
       { _, [station], _ } -> station
-      _ -> :help 
+      _ -> :help
     end
   end
 
